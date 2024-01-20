131a2fadc5f3932d96cf45ea2ad2c9c04bbc5586
Davedelali <daganya922@gmail.com>
2024-01-21 01:55:31 +0700

    email features

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+
+
+
+#handling form views 
+def post_share(request ,post_id):
+    #retrieve post by id
+    post =get_object_or_404(Post, id= post_id, status =Post.Status.PUBLISHED)
+    sent = False
+    form = EmailPostForm(request.POST)
+    
+    if request.method == 'POST':
+        #forms were submitted
+        form = EmailPostForm(request.POST)
+        
+        if form.is_valid():
+            #form feilds pass validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+                     [cd['to']])
+            sent = True
+            #send email
+        else:
+            form = EmailPostForm()
+
+    return render(request , 'blog/post/share.html', {'post': post,
+                                                     'form':form,
+                                                     'sent':sent})        
+        
+
+
+
+#class
+class PostListView(ListView):
+    #alternative post list view
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 # Create your views here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -118,3 +118,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#email configuration for gmail 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'daganya922@gmail.com'
+EMAIL_HOST_PASSWORD = 'omiu doec lmqu bqns'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
