6d896b608ba8abe4573d68daac30bf3a6037660c
Davedelali <daganya922@gmail.com>
2024-01-22 01:26:04 +0700

    updated with commenting system

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post , Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import EmailPostForm
+from .forms import EmailPostForm , CommentForm
+from django.views.decorators.http import require_POST
 
 
 
@@ -12,8 +13,8 @@ def post_share(request ,post_id):
     #retrieve post by id
     post =get_object_or_404(Post, id= post_id, status =Post.Status.PUBLISHED)
     sent = False
+
     form = EmailPostForm(request.POST)
-    
     if request.method == 'POST':
         #forms were submitted
         form = EmailPostForm(request.POST)
@@ -54,7 +55,7 @@ class PostListView(ListView):
 def post_list(request):
      post_list = Post.published.all()
  # Pagination with 3 posts per page
-     paginator = Paginator(post_list, 3)
+     paginator = Paginator(post_list,3)
      page_number = request.GET.get('page', 1)
      try:
         posts = paginator.page(page_number)
@@ -76,9 +77,32 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+     # List of active comments for this post
+     comments = post.comments.filter(active=True)
+ # Form for users to comment
+     form = CommentForm()
      
      
      return render(request,
                  'blog/post/detail.html',
-                 {'post': post})
+                  {'post': post,
+                  'comments': comments,
+                  'form': form})
     
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+ # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+ # Create a Comment object without saving it to the database
+      comment = form.save(commit=False)
+ # Assign the post to the comment
+      comment.post = post
+ # Save the comment to the database
+      comment.save()
+    return render(request, 'blog/post/comment.html',
+                            {'post': post,
+                            'form': form,
+                            'comment': comment})
--- mysite/settings.py
+++ mysite/settings.py
@@ -106,12 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'productionfiles'
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    (BASE_DIR / 'static')
 ]
 
 # Default primary key field type
